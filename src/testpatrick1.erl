67bec839d5639d16edc3644bfdc080ccaaa046aa
unknown <patrick.krogull@CWLAPTOP73.Cordaware.local>
2015-10-09 16:03:46 +0200

    Neuer Branch mit Sequenz

---
+++ src/testpatrick1.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author patrick.krogull
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 09. Okt 2015 16:01
+%%%-------------------------------------------------------------------
+-module(testpatrick1).
+-author("patrick.krogull").
+
+%% API
+-compile(export_all).
+
+create(A) -> lists:seq(1,A).
+
