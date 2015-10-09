abb444b047bd7b60e43907c6c2ff212c987129e5
MaxNeuhauser <MaxNeuhauser@github.com>
2015-10-09 15:14:49 +0200

    Programm1

---
+++ src/testprogramme.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author max.neuhauser
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 09. Okt 2015 10:38
+%%%-------------------------------------------------------------------
+-module(testprogramme).
+-author("max.neuhauser").
+
+-compile(export_all).
+
